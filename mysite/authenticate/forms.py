c6ba671bb0506f5d60e833a1e15545f94bfd1e15
Baillie11 <andrewgbaillie@hotmail.com>
2022-07-12 11:46:51 +1000

    1st commit

---
+++ mysite/authenticate/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', )
+        
